5fd8f2d3212fd9f1104a94177077616edf088845
Isha Viragi <isha.viragi@gmail.com>
2024-08-17 16:21:36 -0400

    Load images as sprites on the canvas to test resolution on website

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-import { Application } from "pixi.js";
+import { Application, Assets, Sprite } from "pixi.js";
 
 (async () => {
   const app = new Application();
+  globalThis.__PIXI_APP__ = app;
+
   await app.init({
     resizeTo: window,
+    backgroundColor: '#4a1850'
   });
   app.canvas.style.position = 'absolute';
   document.body.appendChild(app.canvas);
 
+  const watch_300px = await Assets.load('images/watch-300px.png');
+  const watch_600px = await Assets.load('images/watch-600px.png');
+
+  const sprite_watch300 = Sprite.from(watch_300px);
+  const sprite_watch600 = Sprite.from(watch_600px);
+
+  app.stage.addChild(sprite_watch300);
+  app.stage.addChild(sprite_watch600);
+
+  sprite_watch300.position.set(30, 0)
+  sprite_watch600.position.set(280, 0)
+
 })();
+
+
